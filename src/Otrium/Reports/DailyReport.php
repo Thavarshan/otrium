7dbde3cd6e7419e34ecfa7814a5b9dc752c875ad
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-09 23:01:14 +0530

    Cleanup report classes

--- src/Otrium/Reports/DailyReport.php
+++ src/Otrium/Reports/DailyReport.php
@@ -14,20 +14,4 @@ class DailyReport extends Report implements ReportContract
      * @var string
      */
     protected static $rawStatement = "SELECT id, date, turnover - (turnover * 0.21 / 100) FROM gmv WHERE date BETWEEN  CAST('%s' AS DATE) AND  CAST('%s' AS DATE)";
-
-    /**
-     * Generate the report.
-     *
-     * @param string|null $from
-     *
-     * @return mixed
-     */
-    public function generate(?string $from = null)
-    {
-        [$from, $to] = array_values($this->parseDateRange($from));
-
-        return $this->db->read(
-            sprintf(static::queryStatement(), $to, $from)
-        );
-    }
 }
---
+++ src/Otrium/Reports/Exceptions/ReportException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Otrium\Reports\Exceptions;
+
+use Exception;
+
+class ReportException extends Exception
+{
+}
