9cc115e6ee98d30576975c8d351186a399b5134d
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-09 23:58:40 +0530

    Update Application.php

--- src/Otrium/Core/Application.php
+++ src/Otrium/Core/Application.php
@@ -18,7 +18,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0';
 
     /**
      * The base path for the Laravel installation.
